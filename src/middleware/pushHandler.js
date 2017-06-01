821dcd99fc2167476522a1d3a2a01b88ec0b56f0
Eason Chiang <ishiu.chiang@gmail.com>
2017-06-01 22:51:21 +0800

    CB-6 retrieve user displayName from LINE server if control server didn't send with it

--- src/middleware/pushHandler.js
+++ src/middleware/pushHandler.js
@@ -1,15 +1,24 @@
 const LINEClient = require('../service/LINE/client')
 const lineCarouselTemplate = require('../service/LINE/utils')
   .carouselMessageFormatter
+const emoji = require('node-emoji')
 
 module.exports = () => {
   return async (ctx, next) => {
     const lineClient = new LINEClient(ctx.config)
-    ctx.request.events.forEach(e => {
-      lineClient.pushMessage(
-        e.target,
+    ctx.request.events.forEach(async e => {
+      if (!e.userNickname) {
+        const userProfileResponse = await lineClient.getProfile(e.target)
+        const userProfile = await userProfileResponse.json()
+        e.userNickname = userProfile.displayName
+      }
+      lineClient.pushMessage(e.target, [
+        {
+          type: 'text',
+          text: emoji.emojify(`${e.userNickname}學尢，近日可好啊:wave:`)
+        },
         lineCarouselTemplate(e.altText, e.columns)
-      )
+      ])
     })
   }
 }
