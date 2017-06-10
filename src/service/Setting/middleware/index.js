43c8a3898a3b80bbbafd65c8b348f47daab7f187
Eason Chiang <ishiu.chiang@gmail.com>
2017-06-10 20:17:44 +0800

    CB-19 add index.js for setting middleware

---
+++ src/service/Setting/middleware/index.js
@@ -0,0 +1,5 @@
+const eventAdaptor = require('./eventAdaptor')
+const eventHandler = require('./eventHandler')
+
+module.exports.eventAdaptor = eventAdaptor
+module.exports.eventHandler = eventHandler
