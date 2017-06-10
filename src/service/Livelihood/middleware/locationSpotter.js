7d5e07713aed499ed3e60843382c5cefc2721248
Eason Chiang <ishiu.chiang@gmail.com>
2017-06-10 16:13:19 +0800

    CB-4 received push notification from livelihood in user request

--- src/service/Livelihood/middleware/locationSpotter.js
+++ src/service/Livelihood/middleware/locationSpotter.js
@@ -1,18 +1,36 @@
+const utils = require('../utils')
+
 module.exports = () => {
   return async (ctx, next) => {
     const incomingEvents = ctx.state.incomingEvents
+    const respondEvents = []
     const client = ctx.clients.Livelihood
 
-    const noLocationEvents = incomingEvents.reduce((res, event) => {
+    const noLocationEvents = incomingEvents.reduce(async (res, event) => {
       if (event.type !== 'message') return
 
       const message = event.message
       if (message.type === 'location') {
-        client.post('/notification', {
+        const notificationInfo = await client.post('/notification', {
           latitude: message.latitude,
           longitude: message.longitude
         })
-        ctx.store.onboard.fire(event.source.userId, 'sharedLocation')
+        if (
+          ctx.store.onboard.getUserState(event.source.userId) === 'incoming'
+        ) {
+          ctx.store.onboard.fire(event.source.userId, 'sharedLocation')
+          respondEvents.push({
+            target: event.replyToken,
+            type: 'reply',
+            message: utils.onboardingPushNotification(notificationInfo)
+          })
+        } else {
+          respondEvents.push({
+            target: event.replyToken,
+            type: 'reply',
+            message: utils.userRequestedNotification(notificationInfo)
+          })
+        }
         return res
       } else if (message.type === 'text') {
         // TODO: handle sharing location with text
@@ -21,6 +39,7 @@ module.exports = () => {
       return [...res, event]
     }, [])
 
+    ctx.state.outgoingEvents = [...ctx.state.outgoingEvents, ...respondEvents]
     if (noLocationEvents.length === 0) {
       return
     }
