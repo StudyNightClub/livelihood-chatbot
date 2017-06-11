dc1897075371f0ce8b8d0cb29d03d38e23172f08
Eason Chiang <ishiu.chiang@gmail.com>
2017-06-11 20:50:25 +0800

    CB-22 fix error in keywordSpotter

--- src/service/Livelihood/middleware/keywordSpotter.js
+++ src/service/Livelihood/middleware/keywordSpotter.js
@@ -5,6 +5,16 @@ module.exports = () => {
     const incomingEvents = ctx.state.incomingEvents
     const respondEvents = []
 
+    let mapURL = ''
+    incomingEvents.map(async e => {
+      if (e.message.text === '看看民生預報') {
+        mapURL = await ctx.clients.Livelihood.requestMapButtonURL(
+          e.source.userId
+        )
+      }
+      return Promise.resolve(e)
+    })
+
     const noKeywordEvents = incomingEvents.reduce((res, event) => {
       if (event.type !== 'message') return
 
@@ -25,16 +35,7 @@ module.exports = () => {
             target: event.replyToken,
             event: 'keyword',
             type: 'reply',
-            message: utils.mapButtonMessage(
-              (async () => {
-                const mapURL = await ctx.clients.Livelihood.requestMapButtonURL(
-                  event.source.userId
-                )
-                return mapURL && typeof mapURL === 'string'
-                  ? mapURL
-                  : 'https://www.google.com.tw/maps/place/%E5%8F%B0%E5%8C%97101+No.+7,+Section+5,+Xinyi+Road,+Xinyi+District,+Taipei+City,+110/@25.0339031,121.5623212,17z/data=!3m1!4b1!4m5!3m4!1s0x3442abb6da9c9e1f:0x1206bcf082fd10a6!8m2!3d25.0339639!4d121.5644722'
-              })()
-            )
+            message: utils.mapButtonMessage(mapURL)
           })
           return res
         default:
